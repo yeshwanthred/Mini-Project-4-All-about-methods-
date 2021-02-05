b9782eba8041f33b2cca1ce8289e794abb7bd7c6
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 13:40:21 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-      public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-   System.out.println("\nHey there, how are you?");
-  }
-}
