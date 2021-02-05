ea8e48a38a1ceec1e4b54e63c46508760d0e117a
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 13:51:16 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+//scanner import
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args)
+      scanner scan = new Scanner
+  System.out.println("\nHey there, how are you?");
+  
+   
