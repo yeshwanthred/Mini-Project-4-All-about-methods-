a627ddda24c3215ff369ece5be4e951e261f2a03
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 19:53:19 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,4 @@
-//scanner import
+//Scanner import
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args)
-      scanner scan = new Scanner
-  System.out.println("\nHey there, how are you?");
-  
-   
+
