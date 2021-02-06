966999f35bd0d87a94485624d17f02e18e35bb3b
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 21:30:11 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ class Main {
 
     System.out.println("How many blue and red cars do you see? "); 
   
-  //Get input from client
+  //Get input from client about how many red cars 
 
     int totalCars = scan.nextInt();
 
@@ -22,8 +22,22 @@ class Main {
     int redCars = scan.nextInt();
   
   System.out.println("There are " + blueCars(totalCars, redCars) + " blue cars in garage 3B");
-    //Asking user for speed 
+    //Asking user for how much each parking spot cost  
     System.out.println("You are renting a parking garage? ");
 
+    
     System.out.println("What is the price of 1 parking spot? ");
     int pricePark = scan.nextInt();
+  
+  System.out.println("How many park spots do you want to reserve ? ");
+    int reserve = scan.nextInt();
+
+    System.out.println(" You ordered " + reserve + " parking spots " + "the total cost is $" + totalPrice( pricePark, reserve));
+  }
+ 
+  //The 1st method
+  static int blueCars(int totalCars, int redCars) {
+    //To find the amount of blue cars in a parking lot
+    return totalCars - redCars;
+  }
+ 
