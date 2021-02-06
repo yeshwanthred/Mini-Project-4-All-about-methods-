af4f83d38d6f4601c48652534ce5b928e6004d21
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 20:12:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,4 +3,20 @@ import java.util.Scanner;
 
 class Main {
  public static void main(String[] args) {
+  
+  //Scanner statement
 
+    Scanner scan = new Scanner(System.in);
+     System.out.println("Hey there, welcome to garage 3B");
+ 
+  //Total of red and blue cars
+
+    System.out.println("How many blue and red cars do you see? "); 
+  
+  //Get input from client
+
+    int totalCars = scan.nextInt();
+
+    System.out.println("How many red cars are there?");
+
+    int redCars = scan.nextInt();
