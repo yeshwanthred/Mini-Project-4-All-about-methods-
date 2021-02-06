7ea9c2d4f8097861a0edf72fd15579eca350b77a
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-05 20:23:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,3 +20,10 @@ class Main {
     System.out.println("How many red cars are there?");
 
     int redCars = scan.nextInt();
+  
+  System.out.println("There are " + blueCars(totalCars, redCars) + " blue cars in garage 3B");
+    //Asking user for speed 
+    System.out.println("You are renting a parking garage? ");
+
+    System.out.println("What is the price of 1 parking spot? ");
+    int pricePark = scan.nextInt();
